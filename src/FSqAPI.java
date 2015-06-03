4b1e373b6df65eff2fd215d840dede426c150c61
Aidan Trees <uphairboy@gmail.com>
2015-06-03 13:32:07 -0400

    4sq

--- src/FSqAPI.java
+++ src/FSqAPI.java
@@ -3,6 +3,16 @@ import fi.foyt.foursquare.api.FoursquareApiException;
 import fi.foyt.foursquare.api.Result;
 import fi.foyt.foursquare.api.entities.CompactVenue;
 import fi.foyt.foursquare.api.entities.VenuesSearchResult;
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import javax.json.JsonReader;
+import javax.json.Json;
+
 /**
  * Write a description of class FoursquareApi here.
  * 
@@ -11,13 +21,16 @@ import fi.foyt.foursquare.api.entities.VenuesSearchResult;
  */
 public class FSqAPI
 {
+    protected String client_id = "0UXKNKMOAUUDQZNUEB3FFOZ2DJXIZMNPZQS1UWZXCFFM4KNB";
+    protected String client_secret = "SZTVMMSNH4EUXUWT2TF3UQBMYQJHSLEB54Z2THED5G5AI0QG";
+    protected String version = "20120609";
+
     public void searchVenues(String ll) throws FoursquareApiException {
         // First we need a initialize FoursquareApi. 
-        FoursquareApi foursquareApi = new FoursquareApi("",
-                "", 
-                "http://www.siena.edu");
+        FoursquareApi foursquareApi = new FoursquareApi("0UXKNKMOAUUDQZNUEB3FFOZ2DJXIZMNPZQS1UWZXCFFM4KNB", 
+                "SZTVMMSNH4EUXUWT2TF3UQBMYQJHSLEB54Z2THED5G5AI0QG", "http://www.siena.edu");
 
-        foursquareApi.setVersion("20150601");
+        foursquareApi.setVersion("20120609");
 
         // After client has been initialized we can make queries.
         Result<VenuesSearchResult> result = 
@@ -38,16 +51,55 @@ public class FSqAPI
         }
     }
 
+    public String buildURL(String ll, String query)
+    {
+        String url = String.format("https://api.foursquare.com/v2/venues/search" +
+                "?client_id=%s" +
+                "&client_secret=%s" +
+                "&v=%s" +
+                "&ll=%s" + 
+                "&query=%s", 
+                client_id, client_secret, version, ll, query);
+
+        InputStream in = new URL(url).openStream();
+        JSONObject object = null;
+        try {
+            JsonReader jsonReader = Json.createReader(in);
+            object = jsonReader.readObject();
+            jsonReader.close();
+        }
+        catch(Error e)
+        {
+            System.out.println(e);
+        }
+        finally
+        {
+            in.close();
+        }
+
+        JSONParser parser = new JSONParser();
+        JSONObject response = null;
+        try {
+            response = (JSONObject) parser.parse(object);
+        } catch (ParseException pe) {
+            System.out.println("Error: could not parse JSON response:");
+            System.out.println(object);
+            System.exit(1);
+        }        
+        return jsono.toString();
+    }
+
     public static void main(String[] args)
     {
         FSqAPI test = new FSqAPI();
-        try{
-            test.searchVenues("42.65,-73.75");
-        }
-        catch (FoursquareApiException p)
-        {
-            System.out.println(p);
-        }
-
+        //         try{
+        //             test.searchVenues("42.65,-73.75");
+        //         }
+        //         catch (FoursquareApiException p)
+        //         {
+        //             System.out.println(p);
+        //         }
+        String text = test.buildURL("42.65,-73.75", "burrito");
+        System.out.println(text);
     }
 }
