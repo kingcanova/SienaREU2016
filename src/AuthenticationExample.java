6b63c43352de3fb4f5e68e328925a2fa64afd0d6
Zach <z-ch@users.noreply.github.com>
2015-06-04 09:31:23 -0400

    Update

--- src/AuthenticationExample.java
+++ src/AuthenticationExample.java
@@ -11,8 +11,8 @@ import fi.foyt.foursquare.api.FoursquareApiException;
 public class AuthenticationExample {
 
     public void authenticationRequest(HttpServletRequest request, HttpServletResponse response) {
-        FoursquareApi foursquareApi = new FoursquareApi("0UXKNKMOAUUDQZNUEB3FFOZ2DJXIZMNPZQS1UWZXCFFM4KNB",
-                "SZTVMMSNH4EUXUWT2TF3UQBMYQJHSLEB54Z2THED5G5AI0QG", 
+        FoursquareApi foursquareApi = new FoursquareApi(Secret.FOURSQUARE_CLIENT_ID,
+                Secret.FOURSQUARE_CLIENT_SECRET, 
                 "http://www.siena.edu");
         try {
             // First we need to redirect our user to authentication page. 
@@ -25,8 +25,8 @@ public class AuthenticationExample {
     public void handleCallback(HttpServletRequest request, HttpServletResponse response) {
         // After user has logged in and confirmed that our program may access user's Foursquare account
         // Foursquare redirects user back to callback url. 
-        FoursquareApi foursquareApi = new FoursquareApi("0UXKNKMOAUUDQZNUEB3FFOZ2DJXIZMNPZQS1UWZXCFFM4KNB", 
-        "SZTVMMSNH4EUXUWT2TF3UQBMYQJHSLEB54Z2THED5G5AI0QG", "http://www.siena.edu");
+        FoursquareApi foursquareApi = new FoursquareApi(Secret.FOURSQUARE_CLIENT_ID,
+        Secret.FOURSQUARE_CLIENT_SECRET, "http://www.siena.edu");
         // Callback url contains authorization code 
         String code = request.getParameter("code");
         try {
