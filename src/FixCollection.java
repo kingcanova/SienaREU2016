55aa12df323a2d52a2c7aa685346d7f35d518224
Aidan Trees <uphairboy@gmail.com>
2015-06-18 16:48:53 -0400

    Playing with collection

--- src/FixCollection.java
+++ src/FixCollection.java
@@ -20,9 +20,12 @@ public class FixCollection
         PrintWriter foursquare = new PrintWriter( new File("../TRECData/151_foursquare.csv"));
         PrintWriter yelp = new PrintWriter( new File("../TRECData/151_yelp.csv"));
         PrintWriter tripadvisor = new PrintWriter( new File("../TRECData/151_trip.csv"));
+        PrintWriter webmult = new PrintWriter( new File("../TRECData/151_web.csv"));
         String line =  br.readLine();
         String[] contents = CSVSplitter.split(line, 4);
         ArrayList<Temp> attr = new ArrayList<Temp>(); 
+        ArrayList<Temp> webmultarray = new ArrayList<Temp>(); 
+        ArrayList<String> web = new ArrayList<String>();
         while( contents[1].equals("151") )
         {
             if(contents[2].contains("yelp"))
@@ -49,18 +52,35 @@ public class FixCollection
                     index = contents[2].indexOf("/", index+1);
                     slash++;
                 }
-                attr.add(new Temp(line, url, slash, word_count, len));
+                int end = contents[2].indexOf(".", 7);
+                String site = contents[2].substring(7, end);
+                if(web.contains(site))
+                {
+                    webmultarray.add(new Temp(line, url, slash, word_count, len));
+                }
+                else
+                {
+                    web.add(site);
+                    attr.add(new Temp(line, url, slash, word_count, len));
+                }
             }
             line = br.readLine();
             contents = CSVSplitter.split(line, 4);
         }
         Collections.sort(attr, null);
+        Collections.sort(webmultarray, null);
         for(int i = 0; i < attr.size(); i++)
         {
             pw.print(attr.get(i).toString());
         }
+        for(int i = 0; i < webmultarray.size(); i++)
+        {
+            webmult.print(webmultarray.get(i).toString());
+        }
         pw.close();
         foursquare.close();
         yelp.close();
+        webmult.close();
+        tripadvisor.close();
     }
 }
