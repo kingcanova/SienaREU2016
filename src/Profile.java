ef83ac04cb497ff187e391250d486b6d607e06f9
heritaget <heritaget@hartwick.edu>
2015-07-21 16:06:35 -0400

    added variables to profile

--- src/Profile.java
+++ src/Profile.java
@@ -9,18 +9,21 @@ import java.util.*;
  */
 public class Profile
 {
-    protected int[][] ratings = new int[100][2]; 
-
+    protected int[][] ratings = new int[100][1];
+    protected int user_id;
+    protected double age;
+    protected String group; //Alone,Friends,Family,Other
+    protected String season;//Winter, Summer, Autumn, Spring
+    protected String gender;//male,female
+    protected String duration;//Night out, Day trip, Weekend trip, Longer
+    protected String tripType;//Business, Holiday, Other
+    
     //saves the ratings of each profile example attraction
     protected Hashtable<Integer, Integer> attr_ratings = new Hashtable<Integer, Integer>();
-
     //saves the categories of those attractions and the matching rating
     protected Hashtable<String, Double> cat_count = new Hashtable<String, Double>();
     protected Hashtable<String, Integer> cat_occurance = new Hashtable<String, Integer>();
-    //protected ArrayList<String> posAttrTypes = new ArrayList<String>();
-    //protected ArrayList<String> negAttrTypes = new ArrayList<String>();
 
-    protected int user_id;
     public Profile(int id)
     {
         this.user_id = id;
