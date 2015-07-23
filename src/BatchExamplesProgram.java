c5d3a4e84459aef64cf08b7d07c656e0a278b61c
kd2868 <kdanaher529@gmail.com>
2015-07-23 12:06:26 -0400

    We have ids now

--- src/BatchExamplesProgram.java
+++ src/BatchExamplesProgram.java
@@ -7,39 +7,40 @@ public class BatchExamplesProgram
     public static void main(String[] args)throws Exception 
     {
         BufferedReader br, br2, br3 = null;
-        PrintWriter pw = new PrintWriter("batchExamplesCategorizedWithId.txt");
+        PrintWriter pw = new PrintWriter("batchCollectionCategorizedWithId.txt");
         String examples = "fullBatchExamples.csv";
         String collection = "fullBatchCollection.csv";
-        Hashtable<String, String> table = new Hashtable<String, String>();
+        //Hashtable<String, String> table = new Hashtable<String, String>();
         String line = "";
 
-        br = new BufferedReader(new FileReader(Paths.get("../TRECData/" + examples).toFile()));
-        br2 = new BufferedReader(new FileReader(Paths.get("../TRECData/" + collection).toFile()));
-        br3 = new BufferedReader(new FileReader(Paths.get("./" + "BatchExamplesCategorized.txt").toFile()));
-        while ((line = br.readLine()) != null)
-        {
-            String[] temp = line.split(",");
-            String name = temp[3];
-            String[] trecID = temp[0].split("-");
-            String attId = trecID[1];
-            if(table.get(name)==null)
-                table.put(name, attId);
-        }
-        line = "";
-        while ((line = br2.readLine()) != null)
-        {
-            String[] temp = line.split(",");
-            String name = temp[3];
-            String[] trecID = temp[0].split("-");
-            String attId = trecID[1];
-            if(table.get(name)==null)
-                table.put(name, attId);
-        }
+        //br = new BufferedReader(new FileReader(Paths.get("../TRECData/" + examples).toFile()));
+        br = new BufferedReader(new FileReader(Paths.get("../TRECData/" + collection).toFile()));
+        br3 = new BufferedReader(new FileReader(Paths.get("./" + "BatchCollectionCategorized.txt").toFile()));
+        //br.readLine();
+        int count = 0;
+        int curr = 0;
         while ((line = br3.readLine()) != null)
         {
             String name = line;
             pw.println(name);
-            pw.println(table.get(name));
+
+            String id = br.readLine();
+            String[] temp = CSVSplitter.split(id, 4);
+            String nameEx = temp[3].replace("\"", "");
+            String[] trecID = temp[0].split("-");
+            String attId = trecID[1];
+
+            if(name.equals(nameEx))
+            {
+                pw.println(attId);
+                count++;
+            }
+            else
+            {
+                System.out.println(curr + "\t" + "Categorized: " 
+                                    + "\t" + name + "\n\t" + "Examples: "
+                                    + "\t" + nameEx);
+            }
             line = br3.readLine();
             while( line != null && !line.equals(""))
             {
@@ -47,7 +48,10 @@ public class BatchExamplesProgram
                 line = br3.readLine();
             }
             pw.println();
+            curr++;
         }    
+
+        System.out.println(count);
         pw.close();
         br.close();
         //br2.close();
